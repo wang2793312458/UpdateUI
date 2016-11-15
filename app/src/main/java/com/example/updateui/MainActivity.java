74feddbd059067f94aa085bcd18fe3b6e985b7cf
不知韩号 <2793312458@qq.com>
2016-11-15 20:09:53 +0800

    新增加确认退出对话框

--- app/src/main/java/com/example/updateui/MainActivity.java
+++ app/src/main/java/com/example/updateui/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.updateui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -42,4 +45,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            showMyDialog(); //点击BACK弹出对话框
+        }
+
+        return false;
+    }
+
+    private void showMyDialog() {
+        // 创建退出对话框
+        AlertDialog isExit = new AlertDialog.Builder(this).create();
+        // 设置对话框标题
+        isExit.setTitle("提示");
+        // 设置对话框消息
+        isExit.setMessage("确定要退出吗，未保存的数据将会遗失");
+        // 添加选择按钮并注册监听
+        isExit.setButton("确定", listener);
+        isExit.setButton2("取消", listener);
+        // 显示对话框
+        isExit.show();
+    }
+
+    /**
+     * 监听对话框里面的button点击事件
+     */
+    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case AlertDialog.BUTTON_POSITIVE:// "确认"按钮退出程序
+                    finish();
+                    break;
+                case AlertDialog.BUTTON_NEGATIVE:// "取消"第二个按钮取消对话框
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
